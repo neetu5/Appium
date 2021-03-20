50ec249a7e96d5afb30348417c26f5749836bfa6
neetu.singh <neetu.singh@3pillarglobal.com>
2021-03-21 03:19:28 +0530

    First commit

---
+++ Appium/src/main/java/com/mobileTesting/Appium/DriverSession.java
@@ -0,0 +1,12 @@
+package com.mobileTesting.Appium;
+
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+public class DriverSession {
+
+	public static void main(String[] args) {
+		
+		DesiredCapabilities caps = new DesiredCapabilities();
+	}
+
+}
