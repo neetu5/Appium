b804d21de2e49bbd3f13ffd6af22179751839d0f
neetu.singh <neetu.singh@3pillarglobal.com>
2021-03-21 04:08:28 +0530

    Driver Class Added

--- Appium/src/main/java/com/mobileTesting/Appium/DriverSession.java
+++ Appium/src/main/java/com/mobileTesting/Appium/DriverSession.java
@@ -1,12 +1,32 @@
 package com.mobileTesting.Appium;
 
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import org.openqa.selenium.remote.DesiredCapabilities;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.remote.MobileCapabilityType;
 
 public class DriverSession {
 
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) throws MalformedURLException {
+
 		DesiredCapabilities caps = new DesiredCapabilities();
+		caps.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
+		caps.setCapability(MobileCapabilityType.DEVICE_NAME, "Nexus6New");
+		caps.setCapability(MobileCapabilityType.AUTOMATION_NAME, "UiAutomator2");
+		caps.setCapability(MobileCapabilityType.UDID, "emulator-5554");
+
+		String appURL = System.getProperty("user.dir") + File.separator + "src" + File.separator + "main"
+				+ File.separator + "java" + File.separator + "resources" + File.separator + "ApiDemos-debug.apk";
+
+		caps.setCapability(MobileCapabilityType.APP, appURL);
+		URL appiumUrl = new URL("http://0.0.0.0:4723/wd/hub");
+
+		AppiumDriver driver = new AndroidDriver(appiumUrl, caps);
 	}
 
 }
